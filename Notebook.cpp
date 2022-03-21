aebe37db7dd1af0bc2339e7874575473fa957d9e
eylon <eylonnaamat@gmail.com>
2022-03-21 15:57:56 +0200

    begin note

---
+++ Notebook.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <string>
+#include <stdexcept>
+#include "Notebook.hpp"
+#include "Direction.hpp"
+
+
+void ariel::Notebook::write(uint page, uint row, uint column, ariel::Direction direction, std::string text){
+}
+std::string ariel::Notebook::read(uint page, uint row, uint column, ariel::Direction direction, uint length){
+    return "";
+}
+void ariel::Notebook::erase(uint page, uint row, uint column, ariel::Direction direction, uint length){
+}
+void ariel::Notebook::show(uint page){
+}
+
---
+++ Notebook.hpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <string>
+#include <stdexcept>
+#include "Direction.hpp"
+
+namespace ariel{
+    class Notebook{
+        private:
+            const uint col = 100;
+            uint row;
+            uint pages;
+            // std::vector<std::vector<std::vector<char>>> book;
+
+        public:
+            void write(uint page, uint row, uint column, ariel::Direction direction, std::string text);
+            std::string read(uint page, uint row, uint column, ariel::Direction direction, uint length);
+            void erase(uint page, uint row, uint column, ariel::Direction direction, uint length);
+            void show(uint page);
+    };
+}
